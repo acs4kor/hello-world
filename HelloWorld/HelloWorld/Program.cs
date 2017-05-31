01eb75e96cac9c7d4e84bc61cd179c5790074ee0
Avil Joswin Mascarenhas <kitesavil@gmail.com>
2017-05-31 08:50:22 +0800

    This is used to check how to do add files and commit changes

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -10,6 +10,8 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            Console.WriteLine("This is my first code to Git.\nThis is used to check how to do add files and commit changes");
             Console.ReadLine();
         }
     }
